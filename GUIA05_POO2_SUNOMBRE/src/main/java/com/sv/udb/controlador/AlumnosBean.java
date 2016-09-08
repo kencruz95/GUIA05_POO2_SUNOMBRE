fe4b1c236ba45ecb3e53622c25b63eb40d091a96
jgonzalez <jmgonzalez@rnpn.gob.sv>
2016-09-08 15:51:38 -0600

    Demo Inicial

---
+++ GUIA05_POO2_SUNOMBRE/src/main/java/com/sv/udb/controlador/AlumnosBean.java
@@ -0,0 +1,77 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sv.udb.controlador;
+
+import com.sv.udb.modelo.Alumnos;
+import java.io.Serializable;
+import javax.annotation.PostConstruct;
+import javax.inject.Named;
+import javax.faces.view.ViewScoped;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+/**
+ *
+ * @author REGISTRO
+ */
+@Named(value = "alumnosBean")
+@ViewScoped
+public class AlumnosBean implements Serializable{
+    private Alumnos objeAlum;
+    private boolean guardar;
+
+    public Alumnos getObjeAlum() {
+        return objeAlum;
+    }
+
+    public void setObjeAlum(Alumnos objeAlum) {
+        this.objeAlum = objeAlum;
+    }
+
+    public boolean isGuardar() {
+        return guardar;
+    }
+    
+    /**
+     * Creates a new instance of AlumnosBean
+     */
+    
+    public AlumnosBean() {
+    }
+    
+    @PostConstruct
+    public void init()
+    {
+        this.objeAlum = new Alumnos();
+        this.guardar = true;
+    }
+    
+    public void guar()
+    {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("POOPU");
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+        try
+        {
+            em.persist(this.objeAlum);
+            tx.commit();
+            this.guardar = true;
+        }
+        catch(Exception ex)
+        {
+            tx.rollback();
+            ex.printStackTrace();
+        }
+        finally
+        {
+            em.close();
+            emf.close();            
+        }
+    }
+}
